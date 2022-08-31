651bc77f074a9e80716c6c06136ce3e9eebbf605
Jessica <jessica.thamiresnunes@gmail.com>
2022-08-31 16:33:51 -0300

    Exercícios realizados com sucesso.

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+// Exercício 1
+
+
+nomeUsuario = prompt("Qual é o seu nome?")
+comidaFavorita1 = prompt("Qual sua primeira comida favorita?")
+comidaFavorita2 = prompt("Qual sua segunda comida favorita?")
+comidaFavorita3 = prompt("Qual sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas de ${nomeUsuario}:\n - Comida 1: ${comidaFavorita1}\n - Comida 2: ${comidaFavorita2} \n - Comida 3: ${comidaFavorita3}`)
+//2ª opção
+console.log(`Estas são as comidas favoritas de ${nomeUsuario}:
+- Comida 1: ${comidaFavorita1}
+- Comida 2: ${comidaFavorita2} 
+- Comida 3: ${comidaFavorita3}`)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-//Crie a const para a frase aqui
+
+
+frase ="Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+console.log(frase)
+
+let verdeRosa = frase.replace("verde", "rosa")
+console.log(verdeRosa)
+
+let azulLaranja = frase.replace("azul", "laranja")
+console.log(azulLaranja)
+
+let temVerde = verdeRosa.includes("verde")
+console.log(temVerde)
+
+let temLaranja = azulLaranja.includes("laranja")
+console.log(temLaranja)
+
+minuscula = "Jorge tem uma casa verde e com portão azul, com os dizeres:"
+maiuscula = "\"MAS NÃO DEIXE O GATO SAIR\""
+console.log(minuscula + maiuscula)
